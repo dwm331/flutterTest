7b869238e77242b7943d6acd101331245712aeef
Kathy Kuo <u0124047@gmail.com>
2023-11-10 14:56:20 +0800

    新增地圖套件 google_maps_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'googleMapTest.dart';
 
 void main() {
   runApp(const MyApp());
@@ -111,6 +112,15 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const GoogleMapPageRoute()),
+                );
+              },
+              child: Text("Google Map Page"), 
             ),
           ],
         ),
